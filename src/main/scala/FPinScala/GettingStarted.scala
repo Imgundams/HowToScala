1ebe7c1a089c289630633a54f710c7fac37b2a52
drainhmrc <daniel.raineri@digital.hmrc>
2018-04-25 15:29:06 +0100

    Initial start of FP in scala book

--- src/main/scala/FPinScala/GettingStarted.scala
+++ src/main/scala/FPinScala/GettingStarted.scala
@@ -1,5 +1,59 @@
-package FPinScala
+//Comment
+object GettingStarted {
 
-class GettingStarted {
+  // Absolute
+  def absolute(n:Int): Int = {
+    if (n < 0) -n
+    else n
+  }
 
+  // Factorial
+  def factorial(n: Int): Int = {
+    def looper(n:Int, accumulator:Int): Int =
+      if (n <= 0) accumulator
+      else looper(n-1, n*accumulator)
+    looper(n,1)
+  }
+
+  //  Fibonacci
+  def fibonacci(n:Int):Int = {
+    def looper(n: Int, previously: Int, currently: Int): Int = {
+      if (n == 0) previously
+      else
+        looper(n - 1, currently, currently + previously)
+    }
+    looper(n,0,1)
+  }
+
+  private def formatAbs (x:Int)= {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x,absolute(x))
+  }
+  private def formatFactorial (x:Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(absolute(x), factorial(absolute(x)))
+  }
+  private def formatFibonacci (x:Int) = {
+    val msg = "The fibonacci of %d is %d"
+    msg.format(absolute(x), fibonacci(absolute(x)))
+  }
+  def formatResult(name:String, n: Int, f:Int => Int) ={
+    val msg = "The %s of %d is %d."
+    msg.format(name,n,f(n))
+  }
+
+  def main (arg: Array[String]):Unit ={
+    val n :Int = arg.head.toInt
+    println(  formatAbs(n)
+      +"\n"+
+              formatFactorial(n)
+      +"\n"+
+              formatFibonacci(n)
+      +"\n"+
+              formatResult("Absolute",n,absolute)
+      +"\n"+
+              formatResult("Factorial",n,factorial)
+      +"\n"+
+              formatResult("Fibonacci",n,fibonacci))
+  }
 }
---
+++ src/main/scala/FPinScala/factorial.scala
@@ -0,0 +1,11 @@
+val n = 5
+ def fib(n: Int): Int = {
+    def looper(n: Int, previously: Int, currently: Int):Int =
+    {
+      if (n == 0) previously
+      else
+        looper(n - 1, currently, previously + currently)
+    }
+    looper(n,0,1)
+  }
+println(fib(n))
