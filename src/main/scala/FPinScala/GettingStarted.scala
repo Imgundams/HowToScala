032f2900ba0156ef568b8f7c7372a74513e716a8
drainhmrc <daniel.raineri@digital.hmrc>
2018-05-02 10:42:23 +0100

    FP in scala book and 99 scala problems

--- src/main/scala/FPinScala/GettingStarted.scala
+++ src/main/scala/FPinScala/GettingStarted.scala
@@ -1,59 +1,113 @@
 //Comment
-object GettingStarted {
+import scala.util.Random
+object MyModule {
 
   // Absolute
-  def absolute(n:Int): Int = {
+  def absolute(n: Int): Int = {
     if (n < 0) -n
     else n
   }
 
   // Factorial
   def factorial(n: Int): Int = {
-    def looper(n:Int, accumulator:Int): Int =
+    def looper(n: Int, accumulator: Int): Int =
       if (n <= 0) accumulator
-      else looper(n-1, n*accumulator)
-    looper(n,1)
+      else looper(n - 1, n * accumulator)
+
+    looper(n, 1)
   }
 
   //  Fibonacci
-  def fibonacci(n:Int):Int = {
+  def fibonacci(n: Int): Int = {
     def looper(n: Int, previously: Int, currently: Int): Int = {
       if (n == 0) previously
       else
         looper(n - 1, currently, currently + previously)
     }
-    looper(n,0,1)
+
+    looper(n, 0, 1)
   }
 
-  private def formatAbs (x:Int)= {
+  private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
-    msg.format(x,absolute(x))
+    msg.format(x, absolute(x))
   }
-  private def formatFactorial (x:Int) = {
+
+  private def formatFactorial(x: Int) = {
     val msg = "The factorial of %d is %d"
     msg.format(absolute(x), factorial(absolute(x)))
   }
-  private def formatFibonacci (x:Int) = {
+
+  private def formatFibonacci(x: Int) = {
     val msg = "The fibonacci of %d is %d"
     msg.format(absolute(x), fibonacci(absolute(x)))
   }
-  def formatResult(name:String, n: Int, f:Int => Int) ={
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
     val msg = "The %s of %d is %d."
-    msg.format(name,n,f(n))
+    msg.format(name, n, f(n))
   }
 
-  def main (arg: Array[String]):Unit ={
-    val n :Int = arg.head.toInt
-    println(  formatAbs(n)
-      +"\n"+
-              formatFactorial(n)
-      +"\n"+
-              formatFibonacci(n)
-      +"\n"+
-              formatResult("Absolute",n,absolute)
-      +"\n"+
-              formatResult("Factorial",n,factorial)
-      +"\n"+
-              formatResult("Fibonacci",n,fibonacci))
+  def main(arg: Array[String]): Unit = {
+    println(formatAbs(arg.head.toInt)
+      + "\n" +
+      formatFactorial(arg.head.toInt)
+      + "\n" +
+      formatFibonacci(arg.head.toInt)
+      + "\n" +
+      formatResult("Absolute", arg.head.toInt, absolute)
+      + "\n" +
+      formatResult("Factorial", arg.head.toInt, factorial)
+      + "\n" +
+      formatResult("Fibonacci", arg.head.toInt, fibonacci))
+    println(formatResult("absolute value", -42, absolute))
+    println(formatResult("increment", 7, factorial))
+    println(formatResult("increment", 7, (x: Int) => x + 1))
+    println(formatResult("increment", 7, (x) => x + 1))
+    println(formatResult("increment", 7, x => x + 1))
+    println(formatResult("increment", 7, _ + 1))
+    println(formatResult("increment", 7, x => {val r = x + 1;r}))
+    TestFib
+
+  }
+
+  object TestFib {
+    println("Expected: 0, 1, 1, 2, 3, 5, 8")
+    println("Actual:   %d, %d, %d, %d, %d, %d, %d".format(
+      fibonacci(0), fibonacci(1), fibonacci(2), fibonacci(3), fibonacci(4), fibonacci(5), fibonacci(6)))
+  }
+
+  // Monomorphic function (can only do one type)
+  def binarySearch(ds: Array[Double], key: Double): Int = {
+    //    @annotate.tailrec
+    def go(low: Int, mid: Int, high: Int): Int = {
+      if (low > high) -mid - 1
+      else {
+        val mid2 = (low + high) / 2
+        val d = ds(mid2)
+        if (d == key) mid2
+        else if (d > key) go(low, mid2, mid2 - 1)
+        else go(mid2 + 1, mid2, high)
+      }
+    }
+
+    go(0, 0, ds.length - 1)
+  }
+
+  // Polymorphic function (can do many types)
+  def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
+    @annotation.tailrec
+    def go(low: Int, mid: Int, high: Int): Int = {
+      if (low > high) -mid - 1
+      else {
+        val mid2 = (low + high) / 2
+        val a = as(mid2)
+        val greater = gt(a, key)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
+        else go(mid2 + 1, mid2, high)
+      }
+    }
+    go(0, 0, as.length - 1)
   }
 }
---
+++ src/main/scala/scala99p/ninetyNine.scala
@@ -0,0 +1,78 @@
+// #1 last element in a list
+def last[A](list: List[A]):A = list match {
+    case h :: Nil => h
+    case _ :: tail => last(tail)
+    case _ => throw new Exception
+  }
+
+println(last(List(1,2,3,4,5,6,7,8)))
+
+// #2 last but 1 element in a list
+def penultimate[A](list: List[A]):A = list match {
+    case h :: _ :: Nil => h
+    case _ :: tail => penultimate(tail)
+    case _ => throw new Exception
+  }
+
+println(penultimate(List(1,2,3,4,5,6,7,8)))
+
+// #3 nth element in a list
+def nth[A](element : Int,list: List[A]):A =
+  if (element >= 0) list(element)
+else throw new NoSuchElementException
+
+
+def nthRe[A](n: Int, list: List[A]): A = (n, list) match {
+    case (0, h :: _   ) => h //First element (index 0) also pointed to by line below
+    case (n, _ :: tail) => nthRe(n - 1, tail)
+    case (_, Nil      ) => throw new NoSuchElementException
+  }
+
+println(nth(2, List(1,2,3,4,5,6,7,8))+" and "+nthRe(2, List(1,2,3,4,5,6,7,8)) + " Should be 3")
+
+// #4 length of a list
+def length[A](list: List[A]):Int = {
+  def looper[A](list: List[A], tempLength: Int): Int = {
+    list match {
+      case _ :: Nil => tempLength+1
+      case _ :: tail => looper(tail, tempLength + 1)
+      case _ => throw new Exception
+    }
+  }
+    looper(list, 0)
+}
+
+println(length(List(1,2,3,4,5,6,7,8))+ " Should be " + List(1,2,3,4,5,6,7,8).length)
+
+// #5 Reverse a list
+
+def reverse[A](list: List[A]):List[A]= {
+  def looper[A](list: List[A], listB:List[A]): List[A] = listB match {
+    case Nil => list //End of list
+    case h :: tail => looper(h :: list , tail)
+    case _ => throw new Exception
+  }
+  looper(Nil, list)
+}
+println(reverse(List(1,1,2,3,5,8))+ " Should be "+ List(1,1,2,3,5,8).reverse)
+
+// #6 Find of if a list is Palindrome
+
+def isPalindrome[A](list: List[A]):Boolean = {
+  def reverse[A](list: List[A]): List[A] = {
+    def looper[A](list: List[A], listB: List[A]): List[A] = listB match {
+      case Nil => list //End of list
+      case h :: tail => looper(h :: list, tail)
+      case _ => throw new Exception
+    }
+    looper(Nil, list)
+  }
+  reverse(list)==list
+}
+println(isPalindrome(List(1,1,2,3,5,3,2,1,1))+ " Should be true")
+
+// #7 Flatten a list
+
+def flatten(list: List[Any]):List[Any] = flatten(list)
+
+println(flatten(List(List(1,1),2,List(3,List(5,8))))+ " Should be "+ List(1,1,2,3,5,8))
