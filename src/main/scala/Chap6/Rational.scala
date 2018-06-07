bd4ab6be7a14224ead75f7d7256e9935c433b184
drainhmrc <daniel.raineri@digital.hmrc.gov.uk>
2018-06-07 16:37:09 +0100

    Fin chap 6

--- src/main/scala/Chap6/Rational.scala
+++ src/main/scala/Chap6/Rational.scala
@@ -25,12 +25,136 @@ class Rational4(numerator: Int, denominator: Int) {
 
   def add(that: Rational4): Rational4 =
     new Rational4(
-      numerator * that.denominator +
-        that.numerator * denominator,
+      numerator * that.denominator + that.numerator * denominator,
       denominator * that.denominator)
 
+  // Self References
+  def lessThen(that: Rational4) =
+    this.numerator * that.denominator < that.numerator * this.denominator
+
+  def max(that: Rational4) =
+    if (this.lessThen(that)) that else this
 }
+
+//  Auxiliary Constructors
+
+class Rational5(n: Int, d: Int) {
+
+  require(d != 0)
+
+  val numer: Int = n
+  val denom: Int = d
+
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+
+  override def toString = numer + "/" + denom
+
+  def add(that: Rational5): Rational5 =
+    new Rational5(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+}
+
+// Private fields and methods
+class Rational6(n: Int, d: Int) {
+
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  def add(that: Rational6): Rational6 =
+    new Rational6(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  override def toString = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+}
+
+// Defining Operators
+class Rational7(n: Int, d: Int) {
+
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  def +(that: Rational7): Rational7 =
+    new Rational7(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def *(that: Rational7): Rational7 =
+    new Rational7(numer * that.numer, denom * that.denom)
+
+  override def toString = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+}
+
+// Identifiers in Scala - Literal Identifiers are arbirary Strings enclosed in back ticks ``
+
+// Method Overloading
+ class Rational8(n: Int, d: Int) {
+
+ require(d != 0)
+
+ private val g = gcd(n.abs, d.abs)
+ val numer = n / g
+ val denom = d / g
+
+ def this(n: Int) = this(n, 1)
+
+ def + (that: Rational8): Rational8 =
+ new Rational8(
+ numer * that.denom + that.numer * denom,
+ denom * that.denom
+ )
+
+ def + (i: Int): Rational8 =
+ new Rational8(numer + i * denom, denom)
+
+ def - (that: Rational8): Rational8 =
+ new Rational8(
+ numer * that.denom - that.numer * denom,
+ denom * that.denom
+ )
+
+ def - (i: Int): Rational8 =
+ new Rational8(numer - i * denom, denom)
+
+ def * (that: Rational8): Rational8 =
+ new Rational8(numer * that.numer, denom * that.denom)
+
+ def * (i: Int): Rational8 =
+ new Rational8(numer * i, denom)
+
+ def / (that: Rational8): Rational8 =
+ new Rational8(numer * that.denom, denom * that.numer)
+
+ def / (i: Int): Rational8 =
+ new Rational8(numer, denom * i)
+
+ override def toString = numer + "/" + denom
+
+ private def gcd(a: Int, b: Int): Int =
+ if (b == 0) a else gcd(b, a % b)
+ }
 
 
 new Rational1(1, 2)
 println(new Rational2(3, 4))
+val rational = new Rational8(2,54)
