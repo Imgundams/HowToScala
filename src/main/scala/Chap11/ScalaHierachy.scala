41281ee344887e62e2a78f71a2e9ad0dc9519fb3
drainhmrc <daniel.raineri@digital.hmrc.gov.uk>
2018-07-11 15:06:22 +0100

    Started chapter 11

---
+++ src/main/scala/Chap11/ScalaHierachy.scala
@@ -0,0 +1,11 @@
+package Chap11
+
+class ScalaHierachy {
+//  final def ==(that: Any): Boolean
+//  final def !=(that: Any): Boolean
+//  def equals(that: Any): Boolean
+//  def ##: Int
+//  def hashCode: Int
+//  def toString: String
+
+}
